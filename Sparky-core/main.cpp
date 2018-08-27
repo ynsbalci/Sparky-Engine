8582e44c874427dd772cbe37c03e4fbc751bdf37
YunusBALCI <ynsbalci@outlook.com>
2018-08-27 22:31:05 +0300

    Ep3 Input

---
+++ Sparky-core/main.cpp
@@ -0,0 +1,35 @@
+#include "src/graphics/window.h"
+
+int main()
+{
+	using namespace sparky;
+	using namespace graphics;
+
+	Window window("Sparky!", 960, 540);
+	glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
+
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	while (!window.closed())
+	{
+		window.clear();
+		double x, y;
+		window.getMousePosition(x, y);
+		std::cout << x << ", " << y << std::endl;
+#if 1
+		glBegin(GL_QUADS);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(-0.5f, 0.5f);
+		glVertex2f(0.5f, 0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glEnd();
+#else
+		glDrawArrays(GL_ARRAY_BUFFER, 0, 6);
+#endif
+		window.update();
+	}
+
+	return 0;
+}
