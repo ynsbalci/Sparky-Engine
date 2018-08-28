691c8d9779582975ca69c8010fb8c01c960e028f
YunusBALCI <ynsbalci@outlook.com>
2018-08-28 22:17:27 +0300

    Ep_5_Matrices

--- Sparky-core/src/maths/vec4.h
+++ Sparky-core/src/maths/vec4.h
@@ -9,7 +9,7 @@ namespace sparky {
 		{
 			float x, y, z, w;
 
-			vec4();
+			vec4() = default;
 			vec4(const float& x, const float& y, const float& z, const float& w);
 
 			vec4& add(const vec4& other);
